cc4567415f4df87525f0b409b049437510b28ae0
Harshit <harshitmalik29@gmail.com>
2025-10-18 20:13:36 +0530

    okay now it all is working, now we will continue

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,8 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { JWT_SECRET } from 'backend-common/config';
+import { JWT_SECRET } from '@repo/backend-common/config';
 const wss = new WebSocketServer({ port : 8080});
+
 
 wss.on('connection', function connection(ws, request) {
     const url = request.url;
@@ -11,9 +12,9 @@ wss.on('connection', function connection(ws, request) {
 
     const queryParams =  new URLSearchParams(url.split('?')[1]);
     const token = queryParams.get('token');
-    const decoded = jwt.verify(token, JWT_SECRET);
+    const decoded = jwt.verify(token ?? '', JWT_SECRET);
 
-    if (!decoded || !decoded as JwtPayload.userId){
+    if (!decoded || !(decoded as JwtPayload)?.userId){
         ws.close();
         return;
     }
