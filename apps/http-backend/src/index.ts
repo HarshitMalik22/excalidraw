dbae6cc4264d228279f7807e283633037ce6d134
Harshit <harshitmalik29@gmail.com>
2025-10-18 13:30:47 +0530

    all the pnpm @repo/ and base erros are fixed

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,73 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from '@repo/backend-common/config';
+import { middleware } from "./middleware";
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
+
+const app = express();
+app.use(express.json());
+app.post("/signup", (req, res) => {
+
+    try {
+        const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success){
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+    //db call
+    prismaClient.user.create({
+        data: {
+            email: parsedData.data?.username,
+            password: parsedData.data.password,
+            name: parsedData.data.name
+        } 
+    })
+    res.json({
+        userId: "123",
+    })
+    } catch (e) {
+        res.status(411).json({
+            message: "User already exists with this username" 
+        })
+    }
+})
+
+app.post("/signin", (req, res) => {
+    const data = SigninSchema.safeParse(req.body);
+    if (!data.success){
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    } 
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET)
+    res.json({
+        token
+    })
+})
+
+app.post("/room", middleware, (req, res) => {
+
+    const data = CreateRoomSchema.safeParse(req.body);
+    if (!data.success){
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+
+    //db call
+
+    res.json({
+        roomId: 123
+    })
+})
+
+app.listen(3001);
---
+++ packages/db/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.prismaClient = void 0;
+var extension_1 = require("@prisma/client/extension");
+exports.prismaClient = new extension_1.PrismaClient();
