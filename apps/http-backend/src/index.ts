13a9e48f1176b794a7f826724f4b92b00c6a54c0
Harshit <harshitmalik29@gmail.com>
2025-10-18 21:33:23 +0530

    added hashing in signup route

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -4,29 +4,31 @@ import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from "./middleware";
 import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
 import { prismaClient } from "@repo/db/client";
+import bcrypt from "bcrypt";
 
 const app = express();
 app.use(express.json());
-app.post("/signup", (req, res) => {
-
-    try {
-        const parsedData = CreateUserSchema.safeParse(req.body);
+app.post("/signup", async (req, res) => {
+    const parsedData = CreateUserSchema.safeParse(req.body);
     if (!parsedData.success){
+        console.log(parsedData.error);
         res.json({
             message: "Incorrect inputs"
         })
         return;
     }
     //db call
-    prismaClient.user.create({
+    try{
+        const user = await prismaClient.user.create({
         data: {
             email: parsedData.data?.username,
-            password: parsedData.data.password,
+            //TODO: Hash password
+            password: await bcrypt.hash(parsedData.data.password, 10),
             name: parsedData.data.name
         } 
     })
     res.json({
-        userId: "123",
+        userId: user.id
     })
     } catch (e) {
         res.status(411).json({
@@ -36,8 +38,8 @@ app.post("/signup", (req, res) => {
 })
 
 app.post("/signin", (req, res) => {
-    const data = SigninSchema.safeParse(req.body);
-    if (!data.success){
+    const parsedData = SigninSchema.safeParse(req.body);
+    if (!parsedData.success){
         res.json({
             message: "Incorrect inputs"
         })
